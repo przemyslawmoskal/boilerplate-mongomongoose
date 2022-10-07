c4416807ac50ad4335bdb26efed02c85051e265e
Przemysław Moskal <28692052+przemyslawmoskal@users.noreply.github.com>
2022-10-07 22:25:05 +0200

    Added solution to Challenge 5.

--- myApp.js
+++ myApp.js
@@ -61,8 +61,15 @@ const createManyPeople = (arrayOfPeople, done) => {
   });
 };
 
+// 5) Use model.find() to Search Your Database
+
 const findPeopleByName = (personName, done) => {
-  done(null /*, data*/);
+  Person.find({name: personName}, function(err, data) {
+    if(err) {
+      return console.error(err);
+    }
+    done(null, data);
+  });
 };
 
 const findOneByFood = (food, done) => {
